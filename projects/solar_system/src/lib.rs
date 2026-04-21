510266beb01101c07c03f54d40972711fbad25bf
Ignacio Peralta <losmacanudo2@gmail.com>
2026-04-20 21:32:48 -0300

    Refactor label visibility logic to simplify moon occlusion checks in solar system view

--- projects/solar_system/src/lib.rs
+++ projects/solar_system/src/lib.rs
@@ -1260,8 +1260,7 @@ impl SolarSystem {
                     }
                 } else {
                     if is_sun { element.set_text_content(Some("Sun")); }
-                    let is_moon = self.bodies[data.index].parent.map_or(false, |p| p != 0);
-                    if is_occluded || (data.radius_px < 1.5 && is_moon) || hide_near_parent {
+                    if is_occluded || hide_near_parent {
                         element.style().set_property("display", "none").unwrap();
                     } else {
                         let style = element.style();
